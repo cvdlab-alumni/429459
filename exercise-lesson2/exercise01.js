560641140121bad2ce6bc79aa58fe2596da055f4
V1LL0 <v.cestarelli@gmail.com>
2013-03-06 15:24:38 +0100

    commit dei primi 3 esercizi

---
+++ exercise-lesson2/exercise01.js
@@ -0,0 +1,16 @@
+var riga= "";
+
+for (var i = 1; i < 11; i++) {
+
+for (var j = 1; j < 11; j++) {
+
+	riga = riga + i*j + "\t";
+
+
+};
+
+console.log(riga);
+
+riga="";
+
+};
---
+++ exercise-lesson2/exercise03.js
@@ -0,0 +1,20 @@
+var riga= "";
+
+for (var i = 1; i < 11; i++) {
+
+for (var j = 1; j < 10; j++) {
+	if(i===j){
+	riga = riga + "1," + "\t";
+	}else
+	riga = riga + "0," + "\t";
+
+
+};
+	if(i===j){
+	riga = riga + "1" + "\t";
+	}else
+	riga = riga + "0" + "\t";
+console.log(riga);
+riga="";
+
+};
