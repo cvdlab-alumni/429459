2c1eb28f02ab13142c3a2a98fe5f13cc4ce892c3
V1LL0 <v.cestarelli@gmail.com>
2013-03-21 11:42:23 +0100

    Update exercise03.js

--- learning-javascript/2013-03-08/exercise03.js
+++ learning-javascript/2013-03-08/exercise03.js
@@ -1,5 +1,8 @@
 function toUpper(stringa){
-return stringa.toUpperCase();
+  var a = w.charAt(0);
+  var b = w.splice(1);
+  
+return a.concat(b);
 }
 
 function toUpperSpecial(){return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".toUpperCase();}
